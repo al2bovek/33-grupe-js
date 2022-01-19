7209d56ded51da617fab5db51fc5f13e86770d2a
Aleksandr Dubovik <aleksandr.dubovek@gmail.com>
2022-01-20 00:05:14 +0200

    02-intro

---
+++ 02-intro.js
@@ -0,0 +1,113 @@
+console.log('Kintamųjų palyginimas');
+console.log(' ');
+
+const a = 1;
+const b = 3;
+const c = 9;
+
+if (a>b) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+if (b<c) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+if (a===c) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+if (c===c) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+if (c>=b) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+if (c<=a) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+
+console.log(' ');
+
+const namas = 'namas';
+const rumas = 'rumas';
+const palapine = 'palapine';
+
+const at = namas.length;
+const bt = rumas.length;
+const ct = palapine.length;
+
+console.log(at, bt, ct)
+
+if (at>bt) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+if (bt<ct) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+if (at===ct) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+if (ct!==ct) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+if (ct>=bt) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+if (ct<=at) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+
+console.log(' ');
+
+console.log(' ');
+
+const namai = ['mano namai mano tvirtove'];
+const rumai = ['Traku rumai'];
+const palapines = ['nauduotos bet geros palapines'];
+
+const ata = namai.length;
+const bta = rumai.length;
+const cta = palapines.length;
+
+console.log(ata, bta, cta)
+
+if (at>bt) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+if (bt<ct) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+if (at===ct) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+if (ct!==ct) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+if (ct>=bt) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+if (ct<=at) {
+    console.log('Pomidoras');
+}
+else console.log('Bandykite kitą kartą');
+
+console.log(' ');
+
+console.log('Ciklo for panaudojimas')
+
+console.log(' ');
