4848a84c34f6aaead2e8d7b885b39e4438455d56
Aleksandr Dubovik <aleksandr.dubovek@gmail.com>
2022-01-28 10:53:40 +0200

    js-testas

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log('testas');
+const a=3;
+const b=6;
+if(a>=b);
+else (a<=b);
+console.log(a);
+console.log(b);
+
+for (i=0; i<=10; i++)
+console.log(i)
+
+let x=[1,2,3,4,5,6,7,8,9,10]
+for (i=0; i<=10; i++){
+  
+  if(i%2===0){
+
+  }
+}
+console.log(x);
+
+
+for (i=1; i<=10; i++){
+
+}
